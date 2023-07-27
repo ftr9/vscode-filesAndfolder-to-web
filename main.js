6e214baa981d057f6d99ff67f033f2752aa333b2
ftr9 <ferlandodotel16@gmail.com>
2023-07-27 15:44:14 +0545

    refactor: added default files and folder structure

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import fileChangeCallback from './utils/fileChangeCallback';
+import renderFilesAndFolder from './utils/renderFilesAndFolder';
+import defaultFileAndFolderData from './defaultFileAndFolderData.json';
 const fileInput = document.querySelector('input');
 
 fileInput.addEventListener('change', fileChangeCallback);
+
+//render default structure
+renderFilesAndFolder(defaultFileAndFolderData, document.querySelector('.main'));
